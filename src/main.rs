7c8a6b9aae63b9b2d6a32e37198244ebfa79f4e4
Lokesh Kumar Nalot <lokeshkumar@M1-Mac.local>
2022-02-22 00:40:25 +0530

    init and error handling for cli

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+use std::env;
+use std::net::IpAddr;
+use std::process;
+use std::str::FromStr; //to convert str to ip address type //handles the way our program shuts down
+use std::sync::mpsc::{channel, Sender};
+use std::thread;
+
+struct Args {
+    flag: String,
+    ipaddr: IpAddr,
+    threads: u16,
+}
+
+impl Args {
+    fn new(args: &[String]) -> Result<Args, &'static str> {
+        if args.len() < 2 {
+            return Err("not enough arguments");
+        } else if args.len() > 4 {
+            return Err("too many arguments");
+        }
+        let f = args[2].clone();
+        if let Ok(ipaddr) = IpAddr::from_str(&f) {
+            return Ok(Args {
+                flag: String::from(""),
+                ipaddr,
+                threads: 4,
+            });
+        } else {
+            let flag = args[1].clone();
+            if flag.contains("-h") || flag.contains("-help") && args.len() == 2 {
+                println!(
+                    "Usage: -j to select how many threads you want
+                \r\n      -h or -help to show this help message"
+                );
+                return Err("help");
+            } else if flag.contains("-h") || flag.contains("-help") {
+                return Err("too many arguments");
+            } else if flag.contains("-j") {
+                let ipaddr = match IpAddr::from_str(&args[3]) {
+                    Ok(s) => s,
+                    Err(_) => return Err("not a valid IPADDR; must be IPv4 or IPv6"),
+                };
+                let threads = match args[2].parse::<u16>() {
+                    Ok(s) => s,
+                    Err(_) => return Err("failed to parse thread number"),
+                };
+                return Ok(Args {
+                    threads,
+                    flag,
+                    ipaddr,
+                });
+            } else {
+                return Err("invalid syntax");
+            }
+        }
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    let arguments = Args::new(&args).unwrap_or_else(|err| {
+        if err.contains("help") {
+            process::exit(0);
+        } else {
+            //printing error
+            eprintln!("{} problem parsing arguments: {}", program, err);
+            process::exit(0);
+        }
+    });
+
+    let num_threads = arguments.threads;
+    //instantiating a channel tx = transmitter rx = reciever
+    let (tx, rx) = channel();
+    for i in 0..num_threads {}
+}
